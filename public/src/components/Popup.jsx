eac8146805b3cdf68ee72434d4c3685c575d4a9f
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2023-11-21 18:45:17 +0530

    New Components

---
+++ public/src/components/Popup.jsx
@@ -0,0 +1,9 @@
+const Popup = ({ error }) => {
+    return (
+        <div className="popup-container">
+            <p className='popup'>{error}</p>
+        </div>
+
+    )
+};
+export default Popup;
